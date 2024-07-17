c2b27d3297184f7225f3c130706ef997f7161112
sasmita <boopathyk@Boopathys-MacBook-Air.local>
2024-07-17 14:48:47 +0530

    Added Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from './ui/Home';
 import Menu,{loader as menuLoader} from './features/menu/Menu';
 import Order,{loader as orderLoader} from './features/order/Order';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder,{action as createOrderAction} from './features/order/CreateOrder';
 import Cart from './features/cart/Cart';
 import Error from "./ui/Error";
 import AppLayout from './ui/AppLayout';
@@ -33,7 +33,8 @@ const router = createBrowserRouter([
         },
         {
         path:"/order/new",
-        element:<CreateOrder/>
+        element:<CreateOrder/>,
+        action:createOrderAction
         },
     ]
   }
